0470a12c912c15cd643f1ba518e3f94338d30dcd
zacksleo <zacksleo@gmail.com>
2018-09-14 10:51:07 +0800

    feat(src): add src demo

---
+++ src/Demo.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace zacksleo\demo;
+
+/**
+ * Library Description
+ *
+ * @category  Library
+ * @package   IncomeTax
+ * @author    zacksleo <zacksleo@gmail.com>
+ * @copyright 2018 zacksleo
+ * @license   MIT https://github.com/zacksleo/income-tax/blob/master/LICENSE
+ * @link      https://github.com/zacksleo/income-tax
+ */
+
+class Demo
+{
+
+    /**
+     * Construct function
+     */
+    function __construct()
+    {
+    }
+}
